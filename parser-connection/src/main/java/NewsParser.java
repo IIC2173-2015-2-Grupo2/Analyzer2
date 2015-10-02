13247cdf42189ebacd5437a8d6dbc81aab3933e0
dibpuelma <edib@uc.cl>
2015-10-02 01:22:03 -0300

    Quedan servidos los métodos para pasarle la información al tagger y al saver

--- parser-connection/src/main/java/NewsParser.java
+++ parser-connection/src/main/java/NewsParser.java
@@ -1,41 +1,53 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-import org.json.JSONObject;
+import java.util.*;
 
 public class NewsParser implements NewArrivalListener{
-	Queue<String> newsToParse;
-//	Object parserLock;
+	private Queue<String> newsToParse;
+	private static NewArrivalListener newTaggerListener;
+	private static NewArrivalListener newSaverListener;
+
+	//	Object parserLock;
+
 	public NewsParser(){
 		newsToParse = new LinkedList<>();
-//		parserLock = new Object();
+		//		parserLock = new Object();
 		NewArrivalHandler.createListener(this);
 	}
+
+	public static void createTaggerListener(NewArrivalListener listener){
+		newTaggerListener = listener;
+	}
+
+	public static void createSaverListener(NewArrivalListener listener){
+		newSaverListener = listener;
+	}
+
 	public void newArrival(String s){
-//		synchronized (parserLock) {
-			newsToParse.add(s);
-//		}
+		//		synchronized (parserLock) {
+		newsToParse.add(s);
+		//		}
 		if (newsToParse.size() == 1)
 			parseNews();
 	}
+
 	private void parseNews(){
 		JSONObject jsonObject;
 		jsonObject = new JSONObject(newsToParse.poll());
 		//TODO: Parsear el JSON, entregar el cuerpo al tagger y el resto al DBManager
-		
-//			if(newsToParse.size() > 1){
-//				decomposeJSON();
-//			}
-//			else{
-//				synchronized (parserLock) {
-//					decomposeJSON();
-//				}
-//			}
-//		}
+
+		//			if(newsToParse.size() > 1){
+		//				decomposeJSON();
+		//			}
+		//			else{
+		//				synchronized (parserLock) {
+		//					decomposeJSON();
+		//				}
+		//			}
+		//		}
 	}
-//	private void decomposeJSON(){
-//		JSONObject jsonObject;
-//		jsonObject = new JSONObject(newsToParse.poll());
-//		//TODO: Parsear el JSON
-//	}
+
+	//	private void decomposeJSON(){
+	//		JSONObject jsonObject;
+	//		jsonObject = new JSONObject(newsToParse.poll());
+	//		//TODO: Parsear el JSON
+	//	}
 }
