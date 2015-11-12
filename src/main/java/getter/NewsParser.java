82a2228f76f98d64cede16b57ff87b8d2f31dcb0
dibpuelma <edib@uc.cl>
2015-11-12 17:50:15 -0300

    v1 de la traducción al inglés. También se borró un print que faltaba

--- src/main/java/getter/NewsParser.java
+++ src/main/java/getter/NewsParser.java
@@ -1,6 +1,8 @@
 package getter;
 import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
+
+import org.eclipse.jetty.util.log.Log;
 import org.json.*;
 
 /**
@@ -27,11 +29,16 @@ public class NewsParser {
 	 */
 	public void newArrival(String s){
 		newsToParse = s;
-		parseNews();
+		try {
+			parseNews();
+		} catch (Exception e) {
+
+		}
+
 	}
 
-	//Estructura del json
-	//{ [ {title=>"Titulo", time=>"yyyy-mm-dd hh:mm:ss", header=>"Descripcion", url=>"url a la pagina", body=>"todo el body de la noticia" tags: [ ]},{noticia2},... ] }
+	//Estructura del json language: sp o en
+	//{ [ {title=>"Titulo", time=>"yyyy-mm-dd hh:mm:ss", header=>"Descripcion", url=>"url a la pagina", imageUrl => "url de la imagen", source=> "fuente", body=>"todo el body de la noticia", tags: [ ], language: "sp o en"},{noticia2},... ] }
 	/**
 	 * Se encarga de obtener la información desde el JSON
 	 */
--- src/main/java/tagger/NLP.java
+++ src/main/java/tagger/NLP.java
@@ -7,18 +7,119 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+
 public abstract class NLP {
-	private static ArrayList<String> importantWords;
+	private static ArrayList<String> capitalLetterWords;
 	private static ArrayList<String> listText;
 	private static ArrayList<String> repeatedWords;
 	private static Map<String, Integer> wordCount;
+	private static String language;
 	public static  ArrayList<String> getTags(String text){
 		String innerText = text;
-		importantWords = new ArrayList<String>();
+		language = "sp"; //Cambiar para soportar inglés
+		capitalLetterWords = new ArrayList<String>();
 		repeatedWords = new ArrayList<String>();
 		fillCapitalLetterWords(text);
 		innerText = innerText.toLowerCase();
-		String[] punctuation = {".", ",", ";", ":", "-", "_", "(", ")", "?", "¿", "!", "¡", "'", "<", ">"};
+		String[] punctuation = {".", ",", ";", ":", "-", "_", "(", ")", "?", "¿", "!", "¡", "'", "<", ">"};	
+		for (String word : punctuation) {
+			innerText = innerText.replaceAll(word, "");
+		}
+		innerText = innerText.trim();
+
+		wordCount = findDuplicateString(innerText);
+
+		String[] arrayText = innerText.split(" ");
+		listText = new ArrayList<String>();
+
+		for (String word : arrayText) {
+			listText.add(word);
+		}
+		deleteWords();
+
+		for (String word : listText) {
+			if(wordCount.containsKey(word)){
+				int times = wordCount.get(word);
+				if(times >= 2){
+					repeatedWords.add(word);
+				}
+			}
+		}
+
+		ArrayList<String> outputWords = new ArrayList<String>();
+		outputWords.addAll(repeatedWords);
+		outputWords.addAll(capitalLetterWords);
+		return outputWords;
+		//return createTags(repeatedWords, importantWords);
+	}
+
+//	private ArrayList<Tag> createTags(ArrayList<String> repeatedWords, ArrayList<String> importantWords) {
+//		ArrayList<Tag> tagList = new ArrayList<Tag>();
+//		for (String word : repeatedWords) {
+//			tagList.add(new Tag(word));
+//		}
+//		for (String word : importantWords) {
+//			tagList.add(new Tag(word));
+//		}
+//		return tagList;
+//	}
+	
+	private static void deleteWords(){
+		if(language == "sp"){
+			deleteSpanishWords();
+		}
+		else if (language == "en") {
+			deleteEnglishWords();
+		}
+	}
+	private static void deleteEnglishWords(){
+		String[] articles = {"the", "a", "an", "some"};
+		String[] connectors = {"however", "in", "contrast", "nevertheless", "nonetheless", "yet", "on", "other", "by", "comparison",
+				"contrary", "instead", "any", "case", "all", "same", "likewise", "similarly", "correspondingly", "way", "also", "as",
+				"therefore", "thus", "accordingly", "first", "firstly", "for", "thing", "begin", "with", "without",
+				"second", "secondly", "third", "thridly", "fourth", "foruthly", "fifth", "also", "besides", "addition", "furthermore",
+				"moreover", "finally", "last", "lastly", "most", "importantly", "primarily", "most", "significantly",
+				"essentially", "basically", "particular", "particularly", "specifically", "example", "instance", "illustrat",
+				"say", "namely", "differently", "put", "matter", "fact", "actually", "indeed", "as", "regard", "to", "regards",
+				"concerned", "conclusion", "brief", "summary", "up", "down", "rather", "precise", "afterwards", "later",
+				"meantime", "meanwhile", "anyway", "anyhow", "any", "rate", "therefore", "thus"};
+		String[] personalPronouns = {"I", "me", "mine", "myself", "my", "you", "your", "yours", "yourself", "he", "him", "his", "himself",
+				"she", "her", "hers", "herself", "it", "its", "itself", "we", "us", "our", "ours", "ourselves", "you", "your", "yours", 
+				"yourself", "yourselves", "they", "them", "their", "theirs", "themselves", "thou", "thee", "thy", "thine", "thyself",
+				"who", "whom", "whomself", "whoself", "whose"};
+		String[] extras = {"there", "when", "how", "so", "up", "out", "no", "only", "well", "then", "where",
+				"why", "now", "around", "once", "down", "here", "tonight", "away", "today", "far", "quite", "later", "above", "yet", "maybe", "otherwise",
+				"near", "forward", "somewhere", "anywhere", "please", "forever", "somehow", "absolutely", "abroad", "yeah", "nowhere", "tomorrow", "yesterday",
+				"more", "about", "such", "through", "new", "just", "any", "each", "much", "before", "between", "free", "right", "best", "since",
+				"both", "sure", "without", "back", "better", "enough", "lot", "small", "though", "less", "little", "under", "next", "hard", "real",
+				"left", "least", "short", "last", "within", "along", "lower", "true", "bad", "across", "clear", "easy", "full", "close", "late",
+				"proper", "fast", "wide", "item", "wrong", "ago", "behind", "quick", "straight", "direct", "extra", "morning", "pretty", "overall",
+				"alone", "bright", "flat", "whatever", "slow", "clean", "fresh", "whenever", "cheap", "thin", "cool", "fair", "fine", "smooth", 
+				"false", "thick", "collect", "nearby", "wild", "apart", "none", "aside", "loud", "super", "tight", "honest", "ok", "pray"};
+		String[] preposicions = {"on", "at", "in", "since", "for", "ago", "before", "to", "past", "to", "till", "until", "by", "next",
+				"to", "beside", "under", "below", "over", "above", "across", "through", "into", "towards", "onto", "from", "of", "off",
+				"out", "about"};
+		String[] adverbs = {"not", "also", "often", "too", "usually", "really", "early", "never", "always", "sometimes", "together", "likely",
+				"simply", "generally", "instead", "actually", "again", "rather", "almost", "especially", "ever", "quickly", "probably",
+				"already", "below", "directly", "else", "if", "easily", "eventually", "exactly", "certainly", "normally", "currently",
+				"extremely", "finally", "constantly", "properly", "soon", "specifically", "ahead", "daily", "highly", "inmidiately",
+				"relatively", "slowly", "fairly", "primarily", "completely", "ultimately", "widely", "recently", "seriously", "frequently",
+				"fully", "mostly", "naturally", "nearly", "occasionally", "carefully", "clearly", "essentially", "possible", "slightly", "somewhat",
+				"equally", "greatly", "necessatily", "personally", "rarely", "regularly", "similarly", "basically", "closely", "effectively",
+				"initially", "literally", "mainly", "merely", "gently", "hopefully", "originally", "roughly", "significantly", "totally", "twice",
+				"elsewhere", "everywhere", "obviously", "perfectly", "physically", "succesfully", "suddenly", "truly", "virtually", "altogether",
+				"anyway", "automatically", "deeply", "definitely", "deliberately", "hardly", "readily", "terribly", "unfortunately", "forth", "briefly",
+				"moreover","strongly","honestly", "previously"};
+		String[] demonstratives = {"that", "this", "these", "those"};
+		deleteFromMap(articles);
+		deleteFromMap(personalPronouns);
+		deleteFromMap(connectors);
+		deleteFromMap(extras);
+		deleteFromMap(preposicions);
+		deleteFromMap(adverbs);
+		deleteFromMap(demonstratives);
+	}
+	private static void deleteSpanishWords(){
 		String[] articles = {"el", "la", "los", "las", "un", "unos", "una", "unas"};
 		String[] connectors = {"además","asímismo", "mismo modo", "misma manera", "igualmente", "también", "como", "debido",
 				"gracias","porque", "puesto", "visto", "condición", "así", "entonces", "y"};
@@ -35,21 +136,6 @@ public abstract class NLP {
 				"acaso", "tal"};
 		String[] demonstratives = {"eso", "esto", "aquello", "ese", "este", "aquel", "esta", "esa", "aquella", "estos",
 				"esos", "aquellos", "estas", "esas", "aquellas" };
-		innerText.trim();
-
-		for (String word : punctuation) {
-			innerText = innerText.replace(word, "");
-		}
-
-		wordCount = findDuplicateString(innerText);
-
-		String[] arrayText = innerText.split(" ");
-		listText = new ArrayList<String>();
-
-		for (String word : arrayText) {
-			listText.add(word);
-		}
-
 		deleteFromMap(articles);
 		deleteFromMap(personalPronouns);
 		deleteFromMap(connectors);
@@ -57,34 +143,7 @@ public abstract class NLP {
 		deleteFromMap(preposicions);
 		deleteFromMap(adverbs);
 		deleteFromMap(demonstratives);
-
-		for (String word : listText) {
-			if(wordCount.containsKey(word)){
-				int times = wordCount.get(word);
-				if(times >= 2){
-					repeatedWords.add(word);
-				}
-			}
-		}
-
-		ArrayList<String> outputWords = new ArrayList<String>();
-		outputWords.addAll(repeatedWords);
-		outputWords.addAll(importantWords);
-		return outputWords;
-		//return createTags(repeatedWords, importantWords);
 	}
-
-//	private ArrayList<Tag> createTags(ArrayList<String> repeatedWords, ArrayList<String> importantWords) {
-//		ArrayList<Tag> tagList = new ArrayList<Tag>();
-//		for (String word : repeatedWords) {
-//			tagList.add(new Tag(word));
-//		}
-//		for (String word : importantWords) {
-//			tagList.add(new Tag(word));
-//		}
-//		return tagList;
-//	}
-
 
 	private static void deleteFromMap(String[] wordsToDelete){
 		for (String word : wordsToDelete) {
@@ -96,9 +155,22 @@ public abstract class NLP {
 
 	private static void fillCapitalLetterWords(String text){
 		String[] words = text.split(" ");
+		boolean lastWasCapital = false;
+		String temp = "";
 		for (String word : words) {
-			if(word.matches("^([A-Z][a-z]+)+$"))
-				importantWords.add(word);
+			if(word.matches("^([A-Z][a-z]+)+$") && !lastWasCapital){
+				lastWasCapital = true;
+				temp = word;
+			}
+			else if (word.matches("^([A-Z][a-z]+)+$") && lastWasCapital) {
+				temp = temp + " " + word;
+			}
+			else {
+				lastWasCapital = false;
+			}
+			if (!temp.equals("") && !temp.equals("I")){
+				capitalLetterWords.add(temp);
+			}
 		}
 	}
 
