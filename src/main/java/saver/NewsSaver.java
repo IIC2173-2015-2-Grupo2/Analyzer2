6b1d1761412a9fb96bd5a1739d222c6b093a9d42
samarti <samarti@uc.cl>
2015-11-26 20:04:57 -0300

    Some printing

--- src/main/java/saver/NewsSaver.java
+++ src/main/java/saver/NewsSaver.java
@@ -75,7 +75,7 @@ public class NewsSaver {
 				tags[i] = new Tag(sepTags[i], DataSetType.Tag);
 			saveNewsItemTags(tags, newsItemId);
 		}
-		//System.out.println("Saved news id: " + newsItemId);
+		System.out.println("Saved news id: " + newsItemId);
 		saveProvider(data, newsItemId);
 		return newsItemId;
 	}
@@ -97,6 +97,7 @@ public class NewsSaver {
 				+ "CREATE (b)-[r:`posted`]->(a)\"} ]}", newsId, auxSourceId);
 
 		ClientResponse relationTag = getClientResponse(resource2, relationCreator, encodedBytes);
+		System.out.println("Saved provider id: " + auxSourceId);
 		relationTag.close();
 	}
 
@@ -113,7 +114,7 @@ public class NewsSaver {
 				String tagItem = responseTag.getEntity(String.class);
 				int auxTagId = getIdFromJsonResult(tagItem);
 				responseTag.close();
-
+				System.out.println("Saved tag id: " + auxTagId);
 				String relationCreator = String.format("{\"statements\" : [ {\"statement\" : \""
 						+ "MATCH (a:NewsItem),(b:%s) "
 						+ "WHERE ID(a) = %d AND ID(b) = %d "
